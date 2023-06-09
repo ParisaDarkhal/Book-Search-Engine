9cf2f9e1cfc70350265fed2fb2c8ea1ff862b53c
ParisaDarkhal <p.darkhal@gmail.com>
2023-06-08 20:53:36 -0400

    typeDefs.js created

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ const db = require("./config/connection");
 const path = require("path");
 const db = require("./config/connection");
 const routes = require("./routes");
+const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -20,15 +21,16 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../client/build")));
-}
+// I think this one is not needed in gql
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "../client/build")));
+// }
 
-app.use(routes);
+// app.use(routes);
 
-db.once("open", () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+// db.once("open", () => {
+//   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+// });
 
 // Create a new instance of an Apollo server with the GraphQL schema
 const startApolloServer = async () => {
