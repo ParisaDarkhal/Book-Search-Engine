613b554da8a1f0b735a168448bed7eff32a1102c
ParisaDarkhal <p.darkhal@gmail.com>
2023-06-08 22:36:43 -0400

    me query in resolvers created

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,8 +1,20 @@
-const { User, Book } = require("../models");
+const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
+const { signToken } = require("../utils/auth");
+
+// to fetche the currently logged-in user's data
 const resolvers = {
   Query: {
-    users: async () => {
-      return await User.find({}).populate("");
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id }).select(
+          "-__v -password"
+        ); //excludes __v and poassword from the data like bearer??
+
+        return userData;
+      } else {
+        throw new AuthenticationError("Please Log In");
+      }
     },
   },
 };
